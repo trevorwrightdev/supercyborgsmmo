9024140cf8e102a31e671dedae78299847f4212c
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 14:53:36 -0700

    Adjusted movemenet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,22 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class PlayerMovement : MonoBehaviourPunCallbacks
 {
-    public float speed;
-    Vector2 lastClickedPos;
-    Animator anim;
-    SpriteRenderer rend;
+    public Vector2 lastClickedPos;
     PhotonView view;
-
-    bool moving = false;
 
     private void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        rend = GetComponentInChildren<SpriteRenderer>();
         view = GetComponent<PhotonView>();
     }
 
@@ -30,42 +22,22 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
         }
     }
 
+    // TODO: Make it so your coordinates are stored as integers on your character. But on the movement manager, it converts them to vectors.
+
     void Move()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            moving = true;
-        }
-
-        if (moving && (Vector2)transform.position != lastClickedPos)
-        {
-            // We are moving as long as we haven't reached our destination.
-
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
-        }
-        else
-        {
-            // We are not moving because we have reached our destination.
-            moving = false;
-        }
-
-        // Set animations
-        anim.SetBool("isRunning", moving);
-
-        // Set inversion
-        if (lastClickedPos.x < transform.position.x)
-        {
-            anim.SetBool("isLeft", true);
-        }
-        else if (lastClickedPos.x > transform.position.x)
-        {
-            anim.SetBool("isLeft", false);
+            // TODO: Try all buffered so when a new player joins they get the changes.
+            view.RPC("SetMovePos", RpcTarget.All);
         }
     }
 
-    //TODO: Fix sorting layer issue
+    [PunRPC]
+    void SetMovePos()
+    {
+        lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 
     // If player collides with something we can make moving = false, probably
     // would also have to make it so lastClickedPos = transform.position
