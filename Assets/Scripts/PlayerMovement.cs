7007085390c00fa3aba5210d7f4a3f203efd5ddb
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-27 20:20:29 -0700

    Finished movement system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,14 +12,14 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
     {
         view = GetComponent<PhotonView>();
 
+        // Set the current position to be our actual position on spawn
+        view.RPC("SetTransform", RpcTarget.OthersBuffered, transform.position);
+
         // This is so we don't move to 0, 0 every time we load a new scene
         if (view.IsMine)
         {
-            lastClickedPos = LevelManager.spawnPosition;
-        }
-        else
-        {
-            Debug.Log("Found another player");
+            view.RPC("SetMovePos", RpcTarget.AllBuffered, LevelManager.spawnPosition);
+
         }
     }
 
@@ -46,5 +46,11 @@ public class PlayerMovement : MonoBehaviourPunCallbacks
     void SetMovePos(Vector2 destination)
     {
         lastClickedPos = destination;
+    }
+
+    [PunRPC]
+    void SetTransform(Vector2 newTransform)
+    {
+        transform.position = newTransform;
     }
 }
